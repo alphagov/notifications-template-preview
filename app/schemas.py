d3c8c4b203305b48c81af85a0c8725cc999b132f
Chris Hill-Scott <me@quis.cc>
2017-11-02 09:40:47 +0000

    Factor out schemas
    Splits them into their own file, so they can be managed, and makes the
    function to validate a request against a schema into shared code so
    that it’s not repeated across multiple endpoints.

---
+++ app/schemas.py
@@ -0,0 +1,33 @@
+import jsonschema
+from flask import abort
+
+
+def get_and_validate_json_from_request(request, schema):
+    json = request.get_json()
+    try:
+        jsonschema.validate(json, schema)
+    except jsonschema.ValidationError as exc:
+        abort(400, exc)
+    return json
+
+
+preview_schema = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "description": "schema for parameters allowed when generating a template preview",
+    "type": "object",
+    "properties": {
+        "letter_contact_block": {"type": ["string", "null"]},
+        "values": {"type": ["object", "null"]},
+        "template": {
+            "type": "object",
+            "properties": {
+                "subject": {"type": "string"},
+                "content": {"type": "string"},
+            },
+            "required": ["subject", "content"]
+        },
+        "dvla_org_id": {"type": "string"},
+    },
+    "required": ["letter_contact_block", "template", "values", "dvla_org_id"],
+    "additionalProperties": False,
+}
