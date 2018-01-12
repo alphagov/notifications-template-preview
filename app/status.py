103543215a74bd1b827e8b05cc8bd1a25d4a8ccf
Richard Chapman <richard.chapman@digital.cabinet-office.gov.uk>
2018-01-12 14:30:52 +0000

    Reverted changes to status.py which were used for local testing.

--- app/status.py
+++ app/status.py
@@ -2,6 +2,8 @@ import subprocess
 
 from flask import Blueprint, jsonify
 
+from app import version
+
 status_blueprint = Blueprint('status_blueprint', __name__)
 
 
@@ -9,6 +11,12 @@ status_blueprint = Blueprint('status_blueprint', __name__)
 def _status():
     return jsonify(
         status="ok",
+
+        commit=version.__commit__,
+        build_time=version.__time__,
+        build_number=version.__jenkins_job_number__,
+        build_url=version.__jenkins_job_url__,
+
         ghostscript_version=get_ghostscript_version(),
         imagemagick_version=get_imagemagick_version(),
     ), 200
@@ -20,3 +28,4 @@ def get_imagemagick_version():
 
 def get_ghostscript_version():
     return subprocess.check_output('gs --version', shell=True).decode('utf-8')
+
