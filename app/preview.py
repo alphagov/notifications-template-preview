5cbb2967523e57e18c3d61b75f3aa622e4ec057d
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2018-03-29 11:50:08 +0100

    Transform the colourspace for cmyk pdfs
    - also change how the notify tag is hidden as all precompiled pdfs are 1 page pdfs so API will send a flag to indicate whether or not to hide the notify_tag

--- app/preview.py
+++ app/preview.py
@@ -22,7 +22,9 @@ preview_blueprint = Blueprint('preview_blueprint', __name__)
 # When the background is set to white traces of the Notify tag are visible in the preview png
 # As modifying the pdf text is complicated, a quick solution is to place a white block over it
 def hide_notify_tag(image):
-    with Image(width=130, height=50, background=Color('white')) as cover:
+    with Image(width=130, height=50, background=Color('blue')) as cover:
+        if image.colorspace == 'cmyk':
+            cover.transform_colorspace('cmyk')
         image.composite(cover, left=0, top=0)
 
 
@@ -30,13 +32,17 @@ def hide_notify_tag(image):
 def png_from_pdf(data, page_number, hide_notify=False):
     output = BytesIO()
     with Image(blob=data, resolution=150) as pdf:
-        with Image(width=pdf.width, height=pdf.height, background=Color('white')) as image:
+        with Image(width=pdf.width, height=pdf.height) as image:
             try:
                 page = pdf.sequence[page_number - 1]
             except IndexError:
                 abort(400, 'Letter does not have a page {}'.format(page_number))
 
+            if pdf.colorspace == 'cmyk':
+                image.transform_colorspace('cmyk')
+
             image.composite(page, top=0, left=0)
+            print('hidden', hide_notify)  # noqa
             if hide_notify:
                 hide_notify_tag(image)
             converted = image.convert('png')
@@ -162,9 +168,13 @@ def view_precompiled_letter():
             abort(400)
 
         pdf = base64.decodestring(encoded_string)
+        hide_notify = request.args.get('hide_notify', '') == 'true'
+        print(hide_notify)  # noqa
 
         return send_file(**png_from_pdf(
-            pdf, page_number=int(request.args.get('page', 1)), hide_notify=True
+            pdf,
+            page_number=int(request.args.get('page', 1)),
+            hide_notify=hide_notify
         ))
 
     # catch invalid pdfs
