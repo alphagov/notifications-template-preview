def59430115afafca663370e157c219d4c86b05b
Pea Tyczynska <pea.tyczynska@digital.cabinet-office.gov.uk>
2018-08-24 14:08:13 +0100

    Test the behaviour of 'convert_to_cmyk' on CMYK and RGB input
    This commit serves to document manual tests on 'convert_to_CMYK'
    function.
    
    What was done:
    
    Two pdf files were provided to the function, one contained
    an image in RGB colour mode and the other contained the same image
    in CMYK colour mode. Then outputs have been saved to png files,
    extracted from the docker container using:
    'cp <container-number>:/home/vcap/app/<path-to-file-within-app> .'
    And visually compared for differences. Visually, the structure of
    the output images was the same and displayed colours looked
    almost the same, if not identical.
    
    Later, third test was created to programmatically compare the two
    files, and this test found that the files were not 100% identical.
    I later understood that this was because the RGB input
    and the CMYK input had different margins/cropping.
    
    Still, since the files bear no visible differences, and no unexpected
    changes to CMYK input were made, it was decided that it is ok to
    use 'convert_to_cmyk' method on CMYK input, as well as RGB input.

--- tests/test_transformation.py
+++ tests/test_transformation.py
@@ -1,7 +1,13 @@
 import pytest
+import cv2
+import numpy
 from flask_weasyprint import HTML
 
 from app.transformation import convert_pdf_to_cmyk
+from app.preview import png_from_pdf
+
+from tests.pdf_consts import rgb_image_pdf
+from tests.pdf_consts import cmyk_image_pdf
 
 
 def test_convert_to_cmyk_pdf_first_line_in_header_correct(client):
@@ -22,3 +28,45 @@ def test_subprocess_fails(client, mocker):
         pdf = html.write_pdf()
         convert_pdf_to_cmyk(pdf)
         assert 'ghostscript process failed with return code: 1' in str(excinfo.value)
+
+
+def test_convert_to_cmyk_pdf_on_cmyk(client):
+    input_as_png = png_from_pdf(cmyk_image_pdf, 1)
+    data = convert_pdf_to_cmyk(cmyk_image_pdf)
+    result_as_png = png_from_pdf(data, 1)
+    with open('tests/test_pdfs/input_as_png.png', 'wb') as f:
+        f.write(input_as_png.read())
+
+    with open('tests/test_pdfs/result_as_png.png', 'wb') as f:
+        f.write(result_as_png.read())
+    pass
+
+
+def test_convert_to_cmyk_pdf_on_rgb(client):
+    input_as_png = png_from_pdf(rgb_image_pdf, 1)
+    data = convert_pdf_to_cmyk(rgb_image_pdf)
+    result_as_png = png_from_pdf(data, 1)
+    with open('tests/test_pdfs/rgb_input_as_png.png', 'wb') as f:
+        f.write(input_as_png.read())
+
+    with open('tests/test_pdfs/rgb_result_as_png.png', 'wb') as f:
+        f.write(result_as_png.read())
+    pass
+
+
+def test_convert_to_cmyk_results_same_from_rgb_and_from_cmyk(client):
+    from_rgb_data = convert_pdf_to_cmyk(rgb_image_pdf)
+    from_cmyk_data = convert_pdf_to_cmyk(cmyk_image_pdf)
+    rgb_result_as_png = png_from_pdf(from_rgb_data, 1)
+    cmyk_result_as_png = png_from_pdf(from_cmyk_data, 1)
+
+    with open('tests/test_pdfs/result_from_rgb.png', 'wb') as f:
+        f.write(rgb_result_as_png.read())
+
+    with open('tests/test_pdfs/result_from_cmyk.png', 'wb') as f:
+        f.write(cmyk_result_as_png.read())
+
+    a = cv2.imread('tests/test_pdfs/result_from_rgb.png')
+    b = cv2.imread('tests/test_pdfs/result_from_cmyk.png')
+    difference = cv2.subtract(a, b)
+    assert not numpy.any(difference)
