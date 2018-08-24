d8317ffb6737b7d10eadd445d664005ffab0ff39
Pea Tyczynska <pea.tyczynska@digital.cabinet-office.gov.uk>
2018-08-24 14:08:13 +0100

    Test the behaviour of 'convert_to_cmyk' on CMYK and RGB input
    This commit serves to document manual tests on 'convert_to_CMYK'
    function.
    
    What was done:
    
    Two pdf files were provided to the function, one contained
    an image in RGB colour mode and the other contained the same image
    in CMYK colour mode. Then outputs have been saved to png files,
    extracted from the docker container using:
    'cp <container-number>:/home/vcap/app/<path-to-file-within-app> .'
    And visually compared for differences. Visually, the structure of
    the output images was the same and displayed colours looked
    almost the same, if not identical.
    
    Later, third test was created to programmatically compare the two
    files, and this test found that the files were not 100% identical.
    I later understood that this was because the RGB input
    and the CMYK input had different margins/cropping.
    
    Still, since the files bear no visible differences, and no unexpected
    changes to CMYK input were made, it was decided that it is ok to
    use 'convert_to_cmyk' method on CMYK input, as well as RGB input.

--- tests/pdf_consts.py
+++ tests/pdf_consts.py
@@ -17,3 +17,9 @@ multi_page_pdf = file('tests/test_pdfs/multi_page_pdf.pdf')
 example_dwp_pdf = file('tests/test_pdfs/example_dwp_pdf.pdf')
 
 not_pdf = file(__file__)
+
+cmyk_image_pdf = file('tests/test_pdfs/cmyk_image.pdf')
+
+cmyk_image_pdf = file('tests/test_pdfs/cmyk_image.pdf')
+
+rgb_image_pdf = file('tests/test_pdfs/rgb_image.pdf')
