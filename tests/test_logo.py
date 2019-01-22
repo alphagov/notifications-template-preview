ece3ff0f0319d9d31051726983ad0886556c82af
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2019-01-22 17:04:11 +0000

    Fix code style and simplified tests.

--- tests/test_logo.py
+++ tests/test_logo.py
@@ -1,17 +1,7 @@
 from io import BytesIO
 
-import requests_mock
 
-
-def test_logo(mocker, client, auth_header):
-    with open('tests/test_pdfs/hm-government.svg', 'rb') as f:
-        file = f.read()
-
-    with requests_mock.Mocker() as request_mock:
-        request_mock.get('/{}/static/images/letter-template/hm-government.svg',
-                         body=file,
-                         status_code=200)
-
+def test_logo(mocker, app, client, auth_header):
     response = client.get('/hm-government.svg.png', headers={'Content-type': 'application/json', **auth_header})
 
     with open('tests/test_pdfs/hm-government.png', 'rb') as f:
