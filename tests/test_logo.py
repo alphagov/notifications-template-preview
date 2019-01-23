6c3830d3b4b4a137a85bb1e6c91aba42524aa90e
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2019-01-23 11:17:32 +0000

    Remove exception handling in logo module, since the app has error handlers. Added a test

--- tests/test_logo.py
+++ tests/test_logo.py
@@ -1,10 +1,16 @@
 from io import BytesIO
 
 
-def test_logo(mocker, app, client, auth_header):
+def test_logo(client, auth_header):
     response = client.get('/hm-government.svg.png', headers={'Content-type': 'application/json', **auth_header})
 
     with open('tests/test_pdfs/hm-government.png', 'rb') as f:
         expected_file = f.read()
 
     assert response.get_data() == BytesIO(expected_file).getvalue()
+
+
+def test_logo_returns_404_if_logo_does_not_exist(client, auth_header):
+    response = client.get('/does-not-exist.svg.png', headers={'Content-type': 'application/json', **auth_header})
+
+    assert response.status_code == 404
