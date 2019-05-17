c351b308ef076df1166f85bfd417274b6b7e9d65
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2019-05-17 15:01:52 +0100

    add http healthcheck

--- tests/test_status.py
+++ tests/test_status.py
@@ -11,3 +11,10 @@ def test_status_returns_binary_versions(client):
 
     assert json_data['ghostscript_version'] is not None
     assert json_data['imagemagick_version'] is not None
+
+
+def test_simple_status_returns_ok(client):
+    resp = client.get(url_for('status_blueprint._status', simple='true'))
+
+    assert resp.status_code == 200
+    assert resp.get_data(as_text=True) == 'ok'
