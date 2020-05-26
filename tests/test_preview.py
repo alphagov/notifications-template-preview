c7f9892c2148c3f07619c580cea7c0d87c2755a6
Chris Hill-Scott <me@quis.cc>
2020-05-26 13:38:18 +0100

    Bump utils to 39.4.1
    Changes:
    https://github.com/alphagov/notifications-utils/compare/39.0.1...39.4.1
    
    Bumping specifically to bring in the fix for the linebreak bug in
    letters from https://github.com/alphagov/notifications-utils/pull/741
    
    Requires changes to the hashes of content because of the whitespace
    changes to the HTML that is used to generate templated letter PDFs.

--- tests/test_preview.py
+++ tests/test_preview.py
@@ -119,7 +119,7 @@ def test_get_pdf_caches_with_correct_keys(
     mocked_cache_get,
     mocked_cache_set,
 ):
-    expected_cache_key = 'templated/9338c9b1ffd6a930a8f97299297278742360cf15.pdf'
+    expected_cache_key = 'templated/27329371178f7d942d7a48f62038bb9fe720bc0c.pdf'
     resp = view_letter_template(filetype='pdf')
 
     assert resp.status_code == 200
@@ -145,7 +145,7 @@ def test_get_png_caches_with_correct_keys(
     mocked_cache_get,
     mocked_cache_set,
 ):
-    expected_cache_key = 'templated/9338c9b1ffd6a930a8f97299297278742360cf15.page01.png'
+    expected_cache_key = 'templated/27329371178f7d942d7a48f62038bb9fe720bc0c.page01.png'
     resp = view_letter_template(filetype='png')
 
     assert resp.status_code == 200
@@ -368,7 +368,7 @@ def test_page_count_from_cache(
         }
     )
     assert mocked_cache_get.call_args[0][0] == 'test-template-preview-cache'
-    assert mocked_cache_get.call_args[0][1] == 'templated/0be577c612c720a1d010737faf8a50043feb341b.pdf'
+    assert mocked_cache_get.call_args[0][1] == 'templated/8718b851d26000c7722a0a4c501f60756dcd8ff2.pdf'
     assert response.status_code == 200
     assert json.loads(response.get_data(as_text=True)) == {'count': 10}
 
