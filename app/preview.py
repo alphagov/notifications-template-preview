b6d9feaec81e27dc145a57bbc24d07c7ea589523
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2020-07-17 15:11:05 +0100

    Update all development dependencies
    Includes a tiny change to a log line that wasn't formatted properly
    since this causes an error in the latest version of Pytest.

--- app/preview.py
+++ app/preview.py
@@ -194,7 +194,7 @@ def view_precompiled_letter():
 
     # catch invalid pdfs
     except MissingDelegateError as e:
-        current_app.logger.warning("Failed to generate PDF", str(e))
+        current_app.logger.warning(f"Failed to generate PDF: {e}")
         abort(400)
 
 
