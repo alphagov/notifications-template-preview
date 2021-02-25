21d24317881057f07042eb5b4040e9f97ca5bdc5
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2021-02-25 11:58:49 +0000

    Use FLASK_ENV to avoid startup warnings
    This also enabled automatic reloading when source files change.

--- scripts/run_with_docker.sh
+++ scripts/run_with_docker.sh
@@ -5,6 +5,7 @@ source environment.sh
 
 docker run -it --rm \
   -e NOTIFY_ENVIRONMENT=development \
+  -e FLASK_ENV=development \
   -e STATSD_ENABLED= \
   -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-$(aws configure get aws_access_key_id)} \
   -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-$(aws configure get aws_secret_access_key)} \
