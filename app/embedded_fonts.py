2cb8d1fd32fca0542091a15fe301f190976b8203
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2021-03-09 15:38:22 +0000

    Add filename to log message

--- app/embedded_fonts.py
+++ app/embedded_fonts.py
@@ -40,8 +40,6 @@ def contains_unembedded_fonts(pdf_data):
         walk(obj['/Resources'], fonts, embedded)
 
     unembedded = fonts - embedded
-    if unembedded:
-        current_app.logger.info(f'Found unembedded fonts {[x for x in unembedded]}')
 
     # put things back as we found them
     pdf_data.seek(0)
