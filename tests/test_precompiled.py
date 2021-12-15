6e7ea8d82ded137caed6891d6bfb44a41e6c1582
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2021-12-15 17:31:42 +0000

    Remove incorrect "line spacing" for NOTIFY tag
    This was added in [1] with no explanation. Spacing below a line
    only matters if we have subsequent lines and we want to have a
    bit of vertical spacing between them. But we only have one line.
    
    Shrug.
    
    [1]: https://github.com/alphagov/notifications-template-preview/commit/4940cc1d45885989a5afdb950780c6dcd39846e5#diff-f0481df2f2352b07c8522280d6fb0eec5acacce6e54438b5867893df5af97080R85

--- tests/test_precompiled.py
+++ tests/test_precompiled.py
@@ -112,12 +112,11 @@ def test_add_notify_tag_to_letter_correct_margins(mocker):
     mm_from_top_of_the_page = 4.3
     mm_from_left_of_page = 1.8
     font_size = 6
-    line_spacing = 1.75
 
     x = mm_from_left_of_page * mm
 
     y = float(pdf_original.getPage(0).mediaBox[3]) - (
-        float(mm_from_top_of_the_page * mm + font_size - line_spacing)
+        float(mm_from_top_of_the_page * mm + font_size)
     )
 
     assert len(can.drawString.call_args_list) == 1
