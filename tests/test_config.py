c65f57da7dbc2300f5ed52fafec7119ddddd59d7
Ben Corlett <ben.corlett1@digital.cabinet-office.gov.uk>
2025-04-08 13:33:51 +0100

    Add predefined queue names so celery will not try to create queues

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,6 +4,7 @@ import os
 import pytest
 
 from app import config
+from app.config import QueueNames
 
 
 @pytest.fixture
@@ -28,3 +29,24 @@ def test_load_config(reload_config):
 
     assert os.environ["SECRET_KEY"] == "env"
     assert config.Config.SECRET_KEY == "env"
+
+
+def test_predefined_queues():
+    prefix = "test-prefix-"
+    aws_region = "eu-west-1"
+    aws_account_id = "123456789012"
+
+    class_queues = [
+        value for key, value in vars(QueueNames).items() if not key.startswith("_") and isinstance(value, str)
+    ]
+    predefined_queues = QueueNames.predefined_queues(prefix, aws_region, aws_account_id)
+
+    assert len(predefined_queues) == len(class_queues)
+
+    for queue_name in class_queues:
+        full_queue_name = f"{prefix}{queue_name}"
+        assert full_queue_name in predefined_queues
+        assert (
+            predefined_queues[full_queue_name]["url"]
+            == f"https://sqs.{aws_region}.amazonaws.com/{aws_account_id}/{full_queue_name}"
+        )
